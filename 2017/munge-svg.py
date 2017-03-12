a94fe5a5dc45ddad017577c60c913afeeb7fff54
Richard Barlow <richard@richardbarlow.co.uk>
2017-03-12 14:49:56 +0000

    Implement munge-svg.py '--show-layers' argument

--- 2017/munge-svg.py
+++ 2017/munge-svg.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import re
 import sys
 import argparse
+import lxml.etree as ET
 
 def get_args():
     parser = argparse.ArgumentParser("Munge an SVG into a useful form",
@@ -12,8 +13,8 @@ def get_args():
     parser.add_argument("svg", help="Input SVG to be munged")
     parser.add_argument("--teams", default="teams.txt",
                         help="List of teams to map onto pits")
-    parser.add_argument("--show-layers", nargs="+", default=["Layout"],
-                        help="Show only these layers")
+    parser.add_argument("--show-layers", nargs="+", default=["ALL"],
+                        help="Show only these layers. 'ALL' is special and shows all layers")
     parser.add_argument("--output", default="-",
                         help="Output file. Use - for stdout")
     return parser.parse_args()
@@ -34,9 +35,19 @@ class SVGMunger(object):
         self.layers = layers
 
     def munge(self, src, dst):
-        template = src.read()
+        template = self.select_layers(src.read())
         replaced = re.sub(r'@T_(\d+)', lambda x: self.get_team(x), template)
         dst.write(replaced)
+
+    def select_layers(self, svg):
+        root = ET.fromstring(svg)
+        for layer in root.findall(".//svg:g[@inkscape:label]", root.nsmap):
+            label_attrib = "{{{0}}}label".format(root.nsmap["inkscape"])
+            if "ALL" in self.layers or layer.attrib[label_attrib] in self.layers:
+                layer.attrib["style"] = "display:inline"
+            else:
+                layer.attrib["style"] = "display:none"
+        return ET.tostring(root)
 
     def get_team(self, match):
         num = int(match.group(1))
