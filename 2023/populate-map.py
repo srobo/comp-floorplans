57b06261efcf3ab4816536294eba799cb7b8ca71
Peter Law <PeterJCLaw@gmail.com>
2023-03-25 12:35:20 +0000

    Formalise templating of the pretty venue map

--- 2023/populate-map.py
+++ 2023/populate-map.py
@@ -7,7 +7,7 @@ import yaml
 
 DIR = Path(__file__).parent
 
-MAP_FILE = DIR / 'map.svg'
+MAP_FILE = DIR / 'templates/map.svg'
 TEAMS_FILE = DIR / 'team_names.yaml'
 
 OUTPUT_FILE = DIR / 'output/map-with-teams.svg'
@@ -23,7 +23,7 @@ for idx, name in teams.items():
     if not name:
         name = '—'
 
-    content = content.replace(f'>{idx}<', f'>{name}<')
+    content = content.replace(f'{{{{team-{idx}}}}}', name)
 
 OUTPUT_FILE.parent.mkdir(exist_ok=True, parents=True)
 OUTPUT_FILE.write_text(content)
